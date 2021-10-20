5c990e30e154d20609e4625c95b01892d2f7a14a
Andrew-Stark31 <89913456+Andrew-Stark31@users.noreply.github.com>
2021-10-20 20:30:20 +0530

    Add files via upload

---
+++ linearWithRecursion.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class linearWithRecursion {
+    public static void main(String[] args) {
+        int[] arr={3,2,1,18,9};
+        int target=18;
+        System.out.println(findElement(arr,0,target));
+        System.out.println(findElementRev(arr,arr.length-1,target));
+    }
+    static int findElement(int[] arr,int s,int target)
+    {
+        if(s==arr.length)
+        {
+            return -1;
+        }
+        else if(arr[s]==target)
+        {
+            return s;
+        }
+          return findElement(arr,s+1,target);
+    }
+    static int findElementRev(int[] arr,int s,int target)
+    {
+        if(s==-1)
+        {
+            return -1;
+        }
+        else if(arr[s]==target)
+        {
+            return s;
+        }
+        return findElement(arr,s-1,target);
+    }
+}
